db5ce74b987b5ba1a00a8cbcb7ee9770a1a0aa85
gabe19p <g19purselley@gmail.com>
2025-03-04 16:03:42 +0900

    added side navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// primeng imports
+import { SidebarModule } from 'primeng/sidebar';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarModule, PanelMenuModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'dsrjo-swot';
+
+  items: MenuItem[] = [];
+
+  ngOnInit() {
+    this.items = [
+      { label: 'Dashboard', icon: 'pi pi-home', routerLink: ['/'] },
+      { label: 'SWOT Event', icon: 'pi pi-home', routerLink: ['/events'] },
+      { label: 'Categories', icon: 'pi pi-tags', routerLink: ['/categories'] },
+      { label: 'Profile', icon: 'pi pi-user', routerLink: ['/profile'] },
+    ];
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { CategoriesComponent } from './pages/categories/categories.component';
+import { EventPageComponent } from './pages/event-page/event-page.component';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'categories',
     component: CategoriesComponent,
+  },
+  {
+    path: 'events',
+    component: EventPageComponent,
   },
 ];
